2027420523f0c5fea339fd4beef0989c095fc407
Jack Clackett <jclackett@gmail.com>
2018-09-28 14:44:12 +0200

    remove stream downloads

--- lib/active_storage/service/download_utils.rb
+++ lib/active_storage/service/download_utils.rb
@@ -3,27 +3,27 @@ require 'openssl'
 
 module DownloadUtils
   def stream_download(source, chunk_size = 5_242_880)
-    url = URI.parse(source)
-    http, req = setup_connection(url)
+    # url = URI.parse(source)
+    # http, req = setup_connection(url)
 
-    content_length = http.request_head(url).content_length
-    upper_limit = content_length + (content_length % chunk_size)
-    offset = 0
+    # content_length = http.request_head(url).content_length
+    # upper_limit = content_length + (content_length % chunk_size)
+    # offset = 0
 
-    http.start do |agent|
-      while offset < upper_limit
-        lim = (offset + chunk_size)
-        # QUESTION: is it relevant to set the last chunk
-        # to the exact remaining bytes
-        # lim = content_length if lim > content_length
-        req.range = (offset..lim)
+    # http.start do |agent|
+    #   while offset < upper_limit
+    #     lim = (offset + chunk_size)
+    #     # QUESTION: is it relevant to set the last chunk
+    #     # to the exact remaining bytes
+    #     # lim = content_length if lim > content_length
+    #     req.range = (offset..lim)
 
-        chunk = agent.request(req).body
-        yield chunk.force_encoding(Encoding::BINARY)
+    #     chunk = agent.request(req).body
+    #     yield chunk.force_encoding(Encoding::BINARY)
 
-        offset += chunk_size + 1
-      end
-    end
+    #     offset += chunk_size + 1
+    #   end
+    # end
   end
 
   def download_range(source, range)
--- lib/active_storage/service/version.rb
+++ lib/active_storage/service/version.rb
@@ -1,5 +1,5 @@
 module ActiveStorage
   module CloudinaryService
-    VERSION = '0.3.0'.freeze
+    VERSION = '0.4.0'.freeze
   end
 end
